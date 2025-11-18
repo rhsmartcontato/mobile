4091f30a21cb4a2d0bfdb4b94a801bd729e6d40f
rhsmartcontato <rhsmart.contato@gmail.com>
2025-11-18 00:44:48 +0000

    git

---
+++ src/app/_layout.js
@@ -0,0 +1,10 @@
+import { Stack } from "expo-router";
+import { AppProvider } from "../hooks";
+
+export default function Layout() {
+    return (
+        <AppProvider>
+           <Stack />
+        </AppProvider>
+    );
+}
