b3c891c5f067a5bc5bb87962b406e8463f05f9b7
rhsmartcontato <rhsmart.contato@gmail.com>
2025-11-18 12:08:42 +0000

    a

--- src/app/_layout.js
+++ src/app/_layout.js
@@ -1,10 +1,30 @@
-import { Stack } from "expo-router";
+import { Stack, useSegments, router } from "expo-router";
 import { AppProvider } from "../hooks";
+import { useAuth } from "../hooks/Auth";
+import { useEffect } from "react";
 
 export default function Layout() {
+    const { user } = useAuth();
+    const segments = useSegments();
+
+    useEffect(() => {
+        const inAuthGroup = segments[0] === "(protected)";
+
+        if (!user?.autenticated && inAuthGroup) {
+            router.replace("/");
+        } else {
+            if (user?.autenticated) {
+                router.replace("/(protected)");
+            }
+        }
+    }, [user]);
+
     return (
         <AppProvider>
-           <Stack />
+           <Stack> 
+            <Stack.Screen name="index" />
+            <Stack.Screen name="(protected)" />
+           </Stack>
         </AppProvider>
     );
 }
--- src/app/index.js
+++ src/app/index.js
@@ -1,10 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { useAuth } from '../hooks/Auth';
 
 export default function App() {
+  const { signIn, signOut } = useAuth();
+
+  const handleEntrarSuper = () => {
+    try {
+      
+    } catch (error) {
+      
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>eu não aguento mais esse curso nmrl</Text>
+      <Text style={styles.title}>slk ta funcionando</Text>
+      <Button 
+      title="Signin Super" 
+      onPress={() => 
+      signIn({email: "super@email.com", password: "Super123!"})
+      } 
+      />
+      <Button 
+      title="Signin Adm" 
+      onPress={() => 
+      signIn({email: "adm@email.com", password: "Adm123!"})
+      } 
+      />
+      <Button 
+      title="Signin User" 
+      onPress={() => 
+      signIn({email: "user@email.com", password: "User123!"})
+      } 
+      />
+      <Button title="Signout" onPress={() => signOut()} />
       <StatusBar style="auto" />
     </View>
   );
